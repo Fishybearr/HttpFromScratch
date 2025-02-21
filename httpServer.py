51dbb56a722f9af3df66235a747be89a184f753f
Fishybearr <76235767+Fishybearr@users.noreply.github.com>
2025-02-21 15:16:11 -0500

    add http protocol without headers

--- httpServer.py
+++ httpServer.py
@@ -1,9 +1,17 @@
 import socket
 
 host = '127.0.0.1'
-port = '8888'
+port = 8888
 
 isListening = True
+
+def handle_request(data):
+    response_line = b"HTTP/1.1 200 OK\r\n"
+
+    blank_line = b"\r\n"
+
+    response_body = b"Request received!"
+    return b"".join([response_line,blank_line,response_body])
 
 #Create a socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -16,3 +24,16 @@ print(f"Listening at {s.getsockname()}")
 
 while isListening:
     conn,addr = s.accept()
+
+    print("Connected by",addr)
+    
+    data = conn.recv(1024)
+
+    response = handle_request(data)
+
+    conn.sendall(response)
+
+    conn.close()
+
+
+
