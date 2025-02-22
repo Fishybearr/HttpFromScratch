9e8dfe3dd6bf87b961dff9f51d72cc519f2f9089
Fishybearr <76235767+Fishybearr@users.noreply.github.com>
2025-02-22 15:53:40 -0500

    Add headers to http response

--- httpServer.py
+++ httpServer.py
@@ -8,10 +8,20 @@ isListening = True
 def handle_request(data):
     response_line = b"HTTP/1.1 200 OK\r\n"
 
+    headers = b"".join([
+        b"Server: RAD Server\r\n",
+        b"Content-Type: text/html\r\n"
+        ])
+
     blank_line = b"\r\n"
 
-    response_body = b"Request received!"
-    return b"".join([response_line,blank_line,response_body])
+    response_body = b"""<html> 
+    <body>
+    <h1>Request Reciveed</h1>
+    </body>
+    </html>
+    """
+    return b"".join([response_line,headers,blank_line,response_body])
 
 #Create a socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
